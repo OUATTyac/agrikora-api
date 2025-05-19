174a5f2d0109259a471c894656396b2594e165c6
OUATTyac <61631428+OUATTyac@users.noreply.github.com>
2025-05-19 21:23:20 +0200

    app.py

--- app.py
+++ app.py
@@ -57,6 +57,6 @@ def rapport():
     return send_file("rapport_agrikora.pdf", as_attachment=True)
     
     if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+        import os
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host="0.0.0.0", port=port)
